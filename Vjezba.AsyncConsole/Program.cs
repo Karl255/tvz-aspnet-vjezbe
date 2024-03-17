7dc172c5fde785fa7d8233e10d2d4d0887424fa7
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-03-17 19:16:08 +0100

    Zadatak 1.15

--- Vjezba.AsyncConsole/Program.cs
+++ Vjezba.AsyncConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿// zadatak 1.14
+﻿Console.WriteLine("Zadatak 1.14");
 
 var t1 = CreateSleepTask("task 1", 1000);
 var t2 = CreateSleepTask("task 2", 1500);
@@ -11,3 +11,26 @@ async Task CreateSleepTask(string name, int sleepForMilliseconds) => await Task.
     Thread.Sleep(sleepForMilliseconds);
     Console.WriteLine($"Task '{name}' finished sleeping.");
 });
+
+// -------------------
+
+Console.Out.WriteLine("Zadatak 1.15");
+
+await SleepF1();
+
+async Task SleepF1()
+{
+    Console.WriteLine("SleepF1 started.");
+    await SleepF2();
+    Console.WriteLine("SleepF1 is sleeping...");
+    Thread.Sleep(750);
+    Console.WriteLine("SleepF1 finished sleeping.");
+}
+
+async Task SleepF2()
+{
+    Console.WriteLine("SleepF2 started.");
+    Console.WriteLine("SleepF2 is sleeping...");
+    await Task.Run(() => Thread.Sleep(1000));
+    Console.WriteLine("SleepF2 finished sleeping.");
+}
