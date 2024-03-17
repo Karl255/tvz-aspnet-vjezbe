bcddeb6e54207dfc405d66e47ef9d80f10233466
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-03-17 18:05:33 +0100

    Zadatak 1.3

--- Vjezba.Model/Fakultet.cs
+++ Vjezba.Model/Fakultet.cs
@@ -6,11 +6,23 @@ namespace Vjezba.Model;
 
 public record Fakultet
 {
-	public List<Osoba> Osobe { get; set; } = new();
+	public List<Osoba> Osobe { get; set; } = [];
 
 	public int KolikoProfesora() => Osobe.Count(o => o is Profesor);
 	public int KolikoStudenata() => Osobe.Count(o => o is Student);
 
 	public Student DohvatiStudenta(string jmbag) => Osobe.OfType<Student>().FirstOrDefault(student => student.JMBAG == jmbag);
 	public IEnumerable<Profesor> DohvatiProfesore() => Osobe.OfType<Profesor>().OrderBy(p => p.DatumIzbora);
+
+	public IEnumerable<Student> DohvatiStudente91() => Osobe.OfType<Student>().Where(s => s.DatumRodjenja.Year > 1991);
+	public IEnumerable<Student> DohvatiStudente91NoLinq()
+	{
+		foreach (var osoba in Osobe)
+		{
+			if (osoba is Student student && student.DatumRodjenja.Year > 1991)
+			{
+				yield return student;
+			}
+		}
+	}
 }
