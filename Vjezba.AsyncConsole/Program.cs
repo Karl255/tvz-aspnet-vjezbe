5dc4ee75766b94506e7f1c70884ed460c0d37be2
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-03-17 19:08:30 +0100

    Zadatak 1.14

---
+++ Vjezba.AsyncConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// zadatak 1.14
+
+var t1 = CreateSleepTask("task 1", 1000);
+var t2 = CreateSleepTask("task 2", 1500);
+
+Task.WaitAll(t1, t2);
+
+async Task CreateSleepTask(string name, int sleepForMilliseconds) => await Task.Run(() =>
+{
+    Console.WriteLine($"Task '{name}' is sleeping...");
+    Thread.Sleep(sleepForMilliseconds);
+    Console.WriteLine($"Task '{name}' finished sleeping.");
+});
