5743fd8914e7308c6c8dd041c250506063fb83a2
Karl255 <49595438+Karl255@users.noreply.github.com>
2024-03-17 18:55:19 +0100

    Zadatak 1.12

--- Vjezba.Model/Fakultet.cs
+++ Vjezba.Model/Fakultet.cs
@@ -53,4 +53,6 @@ public record Fakultet
 	private static readonly List<Zvanje> NeaktivniZvanje = [Zvanje.Predavac, Zvanje.VisiPredavac];
 
 	public IEnumerable<Profesor> NeaktivniProfesori(int minPredmeta) => Osobe.OfType<Profesor>().Where(p => NeaktivniZvanje.Contains(p.Zvanje) && p.Predmeti.Count < minPredmeta);
+
+	public IEnumerable<Profesor> AktivniAsistenti(int minPredmeta, int minEcts) => Osobe.OfType<Profesor>().Where(p => p.Zvanje == Zvanje.Asistent && p.Predmeti.Count(predmet => predmet.ECTS >= minEcts) > minPredmeta);
 }
